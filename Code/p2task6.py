9d947d8b01a5462b21f6d95c8216e7993bc1c40a
jiaxuanpang <tjpjx2012@gmail.com>
2019-10-19 16:07:10 -0700

    Add p2task6 and p2task7

--- Code/p2task6.py
+++ Code/p2task6.py
@@ -3,12 +3,13 @@ from module.DimRed import DimRed
 from models import modelFactory
 import argparse
 from module.distanceFunction import distanceFunction
+import sklearn.metrics.pairwise as similarity
 from module.handMetadataParser import getFilelistByID
 import cv2
 import itertools
-from Pathlib import Path
+from pathlib import Path
 from collections import defaultdict
-
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Phase 2 Task 6")
 parser.add_argument(
@@ -52,17 +53,88 @@ decompMethods = DimRed.getSupportedMethods()
 # Two dict,
 # subToImg: subject ID -> image ID
 # imgToSub: image ID -> subject ID
-subToImg, imgToSub = getFilelistByID()
+subToImg, imgToSub = getFilelistByID(metadataPath)
 
 
-# TODO: Test LBP First!!!!!
+decompMethod = 'lda'
+smallest_k = 99999
 
-# Test LBP first
-db = FilesystemDatabase(f"{table}_lbp", create=False)
-model = modelFactory.creatModel()
 
-# subect ID -> features
-subjectFeatures = {}
+db = FilesystemDatabase(f"{table}_cm", create=False)
+model = modelFactory.creatModel('cm')
 
-for keyId in db.keys():
-    subjectFeatures[keyId].append()
+# The imageIdList and featureList should could be mapped to each other.
+target_sub_feat = []
+target_subject_img_lst =  subToImg.get(int(subjectID))
+subject_img_dic_pool = {}
+
+if target_subject_img_lst is None:
+    print("Error! There has no such subject ID: ", subjectID)
+    exit(1)
+
+
+for f_name in target_subject_img_lst:
+    feature = db.getData(f_name)
+    if feature is not None:
+        target_sub_feat.append(
+            model.flattenFecture(model.deserializeFeature(feature), decompMethod)
+        )
+if not target_sub_feat:
+    print("Error! Could not find any images in the database with subject ID: ", subjectID)
+    exit(1)
+
+
+
+for key in db.keys():
+    subject_id = imgToSub.get(key)
+    if subject_img_dic_pool.get(subject_id) is None:
+        subject_img_dic_pool[subject_id] = {
+            'image_list' : [key],
+            'flatten_feature':[],
+            'latentModel':""
+        }
+    else:
+        (subject_img_dic_pool.get(subject_id)['image_list']).append(key)
+
+
+
+if smallest_k>len(target_sub_feat):
+    smallest_k = len(target_sub_feat)
+
+
+for key, value in subject_img_dic_pool.items():
+    temp_feature = []
+    for f_name in value['image_list']:
+        feature = db.getData(f_name)
+        if feature is not None:
+            temp_feature.append(
+                model.flattenFecture(model.deserializeFeature(feature), decompMethod)
+            )
+    value['flatten_feature'] = temp_feature
+    if smallest_k > len(value['flatten_feature']):
+        smallest_k = len(value['flatten_feature'])
+
+target_latent_model = DimRed.createReduction(decompMethod, k=smallest_k, data=target_sub_feat)
+flattend_target_latent_model = np.reshape(target_latent_model.getLatentSemantics(),(1,-1))
+
+
+simPair = []
+for key,value in subject_img_dic_pool.items():
+    value['latentModel'] = DimRed.createReduction(decompMethod, k= smallest_k, data=value['flatten_feature'] )
+    flattend_target_latent_pool_model = np.reshape(value['latentModel'].getLatentSemantics(), (1,-1))
+    #sim = similarity.cosine_similarity(flattend_target_latent_model,flattend_target_latent_pool_model)
+    sim = np.dot(flattend_target_latent_model,flattend_target_latent_pool_model.T)
+    simPair.append((key,sim[0][0]))
+
+simPair = sorted(simPair,key = lambda x: x[1],reverse=True)
+print("Top 3 Subject ID: ",simPair[0][0],simPair[1][0],simPair[2][0])
+
+image1 = cv2.imread("dataset/"+subject_img_dic_pool[simPair[0][0]]['image_list'][0]+'.jpg')
+image2 = cv2.imread("dataset/"+subject_img_dic_pool[simPair[1][0]]['image_list'][0]+'.jpg')
+image3 = cv2.imread("dataset/"+subject_img_dic_pool[simPair[2][0]]['image_list'][0]+'.jpg')
+cv2.imshow("top1",image1)
+cv2.imshow("top2",image2)
+cv2.imshow("top3",image3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Code/p2task7.py
+++ Code/p2task7.py
@@ -1,32 +1,106 @@
-import argparse
-import numpy as np
-from module.reader import RGBtoArrayReader
+from module.database import FilesystemDatabase
 from module.DimRed import DimRed
-from sklearn.metrics.pairwise import cosine_similarity
+from models import modelFactory
+import argparse
+from module.distanceFunction import distanceFunction
+import sklearn.metrics.pairwise as similarity
+from module.handMetadataParser import getFilelistByID
+import cv2
+import itertools
+from pathlib import Path
+from collections import defaultdict
+import numpy as np
 
-# Get necessary arguments
-parser = argparse.ArgumentParser(description="Phase 2 Task 7")
-parser.add_argument("-k", "--topk", metavar="topk", type=int, help="K.", required=True)
+parser = argparse.ArgumentParser(description="Phase 2 Task 6")
 parser.add_argument(
-    "-img",
-    "--image_dir",
-    metavar="image_dir",
+    "-t",
+    "--table",
+    metavar="table",
     type=str,
-    help="The directory of the images",
+    help="The table will be used.",
+    required=True,
+)
+parser.add_argument("-k", "--topk", metavar="topk", type=int, help="K.", required=True)
+
+parser.add_argument(
+    "-meta",
+    "--metadata",
+    metavar="METADATA_PATH",
+    type=str,
+    help="Path of metadata.",
     required=True
 )
+
 args = parser.parse_args()
-topk = args.topk
-image_directory = args.image_dir
 
-# Get all the images from the directory and construct 1D array of each image
-RAR = RGBtoArrayReader(image_directory, flatten=True)
-flatten_images_array = np.array([array for array in RAR])
+# extract argument
+table = args.table.lower()
+k = args.topk
+metadataPath = Path(args.metadata)
 
-# Build the similarity matrix
-similarity_matrix = cosine_similarity(flatten_images_array)
-images = RAR.get_parsed_image_list()
+# Get all models and dimention reduction methods
+models = modelFactory.getSupportModel()
+decompMethods = DimRed.getSupportedMethods()
 
-# Perfrom NMF process and print results
-nmf = DimRed.createReduction('nmf', k=topk, data=similarity_matrix)
-nmf.printLatentSemantics(images, similarity_matrix)
+# Two dict,
+# subToImg: subject ID -> image ID
+# imgToSub: image ID -> subject ID
+subToImg, imgToSub = getFilelistByID(metadataPath)
+
+
+decompMethod = 'lda'
+smallest_k = 99999
+
+
+db = FilesystemDatabase(f"{table}_cm", create=False)
+model = modelFactory.creatModel('cm')
+
+# The imageIdList and featureList should could be mapped to each other.
+subject_img_dic_pool = {}
+
+
+
+for key in db.keys():
+    subject_id = imgToSub.get(key)
+    if subject_img_dic_pool.get(subject_id) is None:
+        subject_img_dic_pool[subject_id] = {
+            'image_list' : [key],
+            'flatten_feature':[],
+            'latentModel':""
+        }
+    else:
+        (subject_img_dic_pool.get(subject_id)['image_list']).append(key)
+
+
+for key, value in subject_img_dic_pool.items():
+    temp_feature = []
+    for f_name in value['image_list']:
+        feature = db.getData(f_name)
+        if feature is not None:
+            temp_feature.append(
+                model.flattenFecture(model.deserializeFeature(feature), decompMethod)
+            )
+    value['flatten_feature'] = temp_feature
+    if smallest_k > len(value['flatten_feature']):
+        smallest_k = len(value['flatten_feature'])
+
+
+
+sub_sub_sim_matrix = np.zeros((len(subject_img_dic_pool.keys()),len(subject_img_dic_pool.keys())))
+
+
+for key,value in subject_img_dic_pool.items():
+    value['latentModel'] = DimRed.createReduction(decompMethod, k= smallest_k, data=value['flatten_feature'] )
+    flattend_target_latent_pool_model = np.reshape(value['latentModel'].getLatentSemantics(), (1,-1))
+
+
+for i, (key,value) in enumerate(subject_img_dic_pool.items()):
+    for j, (key_1, value_1) in enumerate(subject_img_dic_pool.items()):
+        #sim = similarity.cosine_similarity(flattend_target_latent_model,flattend_target_latent_pool_model)
+        sub_sub_sim_matrix[i][j] = np.dot( np.reshape(value['latentModel'].getLatentSemantics(), (1,-1)), np.reshape(value_1['latentModel'].getLatentSemantics(), (1,-1)).T)
+
+
+
+
+nmf_obj = DimRed.createReduction("nmf", k= k, data=sub_sub_sim_matrix )
+nmf_obj.printLatentSemantics(list(subject_img_dic_pool.keys()), sub_sub_sim_matrix, "")
